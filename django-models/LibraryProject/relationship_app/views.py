f858c1a672c9b753c5574b22bce0d6b0f8835226
Nwosu Joseph Ifeanyi <jokechi2991@gmail.com>
2025-10-24 09:23:59 +0100

    Fix string quotes and import in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library   # make sure Library is imported
+from .models import Book, Library
 
-# Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view: show details of a library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"  # <-- checker wants full path
-    context_object_name = "library"
-
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
