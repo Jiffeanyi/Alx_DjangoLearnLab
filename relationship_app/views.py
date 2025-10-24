9aa7d4a317a62c20cf72ec6831d8d9deedf0a992
Nwosu Joseph Ifeanyi <jokechi2991@gmail.com>
2025-10-24 08:50:39 +0000

    Added explicit Library model import in views.py

--- relationship_app/views.py
+++ relationship_app/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library, Book
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
-
-from django.views.generic.detail import DetailView
-from .models import Library
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
