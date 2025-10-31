ee293bad68bd8659d98e0afff28a22a6d31df543
Nwosu Joseph Ifeanyi <jokechi2991@gmail.com>
2025-10-31 19:37:02 +0000

    Implemented templates and linked views

--- relationship_app/views.py
+++ relationship_app/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
 
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, 'relationship_app/view_name.html')
 
 
-class LibraryDetailView(DetailView):
+from django.views.generic.detail import DetailView
+
+class LibraryDetailView(DetailView):  
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
