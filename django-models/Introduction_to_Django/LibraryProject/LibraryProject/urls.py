5a7b0b00448fb621196572a803645920690f9207
Nwosu Joseph Ifeanyi <jokechi2991@gmail.com>
2025-11-02 10:21:16 +0000

    Added relationship_app with views, URLs, and templates for books and libraries

--- django-models/Introduction_to_Django/LibraryProject/LibraryProject/urls.py
+++ django-models/Introduction_to_Django/LibraryProject/LibraryProject/urls.py
@@ -1,6 +1,6 @@
 """
 URL configuration for LibraryProject project.
- b  
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
+    path("admin/", admin.site.urls),
 ]
-
--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "bookshelf",
-    
     'relationship_app',
 ]
 
@@ -123,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/books/'
+LOGOUT_REDIRECT_URL = '/login/'
